84fb5583aeaa4f5aae9d5a0459966af71a2f4751
Punith <punith.gowda.s@accenture.com>
2026-03-10 23:32:16 +0530

    removing excess files

--- terraform/remediation/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "myRg1"
-    storage_account_name = "teststaccn022002testing"
-    container_name       = "tfstate"
-    key                  = "remediation.tfstate"  # Separate state file for remediation
-  }
-}
